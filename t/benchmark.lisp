a3985fc9a2989b8678b705514a2b9d7c877f0226
Eitaro Fukamachi <e.arrows@gmail.com>
2014-10-10 14:46:16 +0900

    Add a benchmark for 'make-http-parser'.

--- t/benchmark.lisp
+++ t/benchmark.lisp
@@ -1,20 +1,37 @@
 (in-package :cl-user)
-(defpackage fast-http-test.bench
+(defpackage fast-http-test.benchmark
   (:use :cl
         :fast-http)
-  (:export :run-benchmark
+  (:export :run-ll-benchmark
+           :run-benchmark
+           :run-ll-profile
            :run-profile))
-(in-package :fast-http-test.bench)
+(in-package :fast-http-test.benchmark)
 
 (syntax:use-syntax :interpol)
 
-(defun run-benchmark ()
+(defun run-ll-benchmark ()
   (let ((parser (make-parser))
         (callbacks (make-parser-callbacks))
         (data (babel:string-to-octets #?"GET /cookies HTTP/1.1\r\nHost: 127.0.0.1:8090\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\nCookie: name=wookie\r\n\r\n")))
     (time
      (loop repeat 100000 do
        (http-parse parser callbacks data)))))
+
+(defun run-benchmark ()
+  (let* ((data (babel:string-to-octets #?"GET /cookies HTTP/1.1\r\nHost: 127.0.0.1:8090\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\nCookie: name=wookie\r\n\r\n")))
+    (time
+     (loop repeat 100000 do
+       (let* ((http (make-http-request))
+              (parser (make-http-parser http)))
+         (funcall parser data))))))
+
+#+sbcl
+(defun run-ll-profile ()
+  (sb-profile:profile "FAST-HTTP" "FAST-HTTP.ERROR" "FAST-HTTP.BYTE-VECTOR" "FAST-HTTP.UTIL")
+  (run-ll-benchmark)
+  (sb-profile:report)
+  (sb-profile:unprofile "FAST-HTTP" "FAST-HTTP.ERROR" "FAST-HTTP.BYTE-VECTOR" "FAST-HTTP.UTIL"))
 
 #+sbcl
 (defun run-profile ()
