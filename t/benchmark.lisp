1eda2a9c0086129093fede459370011c66c329a5
Eitaro Fukamachi <e.arrows@gmail.com>
2014-10-31 02:53:12 +0900

    FAST-HTTP.URL doesn't exist anymore.

--- t/benchmark.lisp
+++ t/benchmark.lisp
@@ -30,7 +30,7 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defparameter *profile-packages*
-    '("FAST-HTTP" "FAST-HTTP.PARSER" "FAST-HTTP.BYTE-VECTOR" "FAST-HTTP.URL" "FAST-HTTP.ERROR" "FAST-HTTP.UTIL")))
+    '("FAST-HTTP" "FAST-HTTP.PARSER" "FAST-HTTP.BYTE-VECTOR" "FAST-HTTP.ERROR" "FAST-HTTP.UTIL")))
 
 #+sbcl
 (defun run-ll-profile ()
